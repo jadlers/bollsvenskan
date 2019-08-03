fbb6d480b4a5b3271f477eee0a366068b4d28af7
Jacob Adlers <jacob.adlers@gmail.com>
2019-08-03 23:59:43 +0200

    Add Snackbar with message when saving new match

--- client/src/components/NewMatchForm.js
+++ client/src/components/NewMatchForm.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from "react";
 
 import Button from "@material-ui/core/Button";
+import Slide from "@material-ui/core/Slide";
+import Snackbar from "@material-ui/core/Snackbar";
 
 import TeamFields from "./TeamFields";
 
+const SlidingTransition = props => <Slide {...props} direction="down" />;
+
 const NewMatchForm = () => {
+  const [snack, setSnack] = useState({ isOpen: false, message: "" });
+  const handleCloseSnack = () => setSnack({ ...snack, isOpen: false });
+
   const [team1Players, setTeam1Players] = useState([]);
   const [team2Players, setTeam2Players] = useState([]);
 
@@ -50,42 +57,63 @@ const NewMatchForm = () => {
 
     const baseUrl = process.env.REACT_APP_API_URL;
     try {
-      // TODO: Show feedback to user
-      await fetch(`${baseUrl}/match`, {
+      const res = await fetch(`${baseUrl}/match`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
       });
+
+      if (res.ok) {
+        setSnack({
+          ...snack,
+          isOpen: true,
+          message: "Successfully saved match",
+        });
+      } else {
+        setSnack({ ...snack, isOpen: true, message: "Failed to save match" });
+      }
     } catch (error) {
+      setSnack({ ...snack, isOpen: true, message: "Failed to save match" });
       console.log("Error:", error);
     }
   };
 
   return (
-    <form action="post">
-      <TeamFields
-        teamNum={1}
-        teamScore={team1Score}
-        setTeamScore={setTeam1Score}
-        players={players}
-        opponents={team2Players}
-        handlePlayerUpdate={handlePlayerUpdate(team1Players, setTeam1Players)}
+    <>
+      <Snackbar
+        open={snack.isOpen}
+        onClose={handleCloseSnack}
+        autoHideDuration={5000}
+        anchorOrigin={{ horizontal: "center", vertical: "top" }}
+        TransitionComponent={SlidingTransition}
+        message={snack.message}
       />
-      <TeamFields
-        teamNum={2}
-        teamScore={team2Score}
-        setTeamScore={setTeam2Score}
-        players={players}
-        opponents={team1Players}
-        handlePlayerUpdate={handlePlayerUpdate(team2Players, setTeam2Players)}
-      />
-      <div />
-      <Button variant="contained" color="primary" onClick={handleSubmit}>
-        Spara
-      </Button>
-    </form>
+
+      <form action="post">
+        <TeamFields
+          teamNum={1}
+          teamScore={team1Score}
+          setTeamScore={setTeam1Score}
+          players={players}
+          opponents={team2Players}
+          handlePlayerUpdate={handlePlayerUpdate(team1Players, setTeam1Players)}
+        />
+        <TeamFields
+          teamNum={2}
+          teamScore={team2Score}
+          setTeamScore={setTeam2Score}
+          players={players}
+          opponents={team1Players}
+          handlePlayerUpdate={handlePlayerUpdate(team2Players, setTeam2Players)}
+        />
+        <div />
+        <Button variant="contained" color="primary" onClick={handleSubmit}>
+          Spara
+        </Button>
+      </form>
+    </>
   );
 };
 
