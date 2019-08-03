5556d853208880d9bc5fa5f28429c3d42dd7a167
Jacob Adlers <jacob.adlers@gmail.com>
2019-08-03 23:14:45 +0200

    Extract team inputs to its own component

--- client/src/components/NewMatchForm.js
+++ client/src/components/NewMatchForm.js
@@ -1,17 +1,14 @@
 import React, { useState, useEffect } from "react";
 
 import Button from "@material-ui/core/Button";
-import Checkbox from "@material-ui/core/Checkbox";
-import FormControl from "@material-ui/core/FormControl";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import FormGroup from "@material-ui/core/FormGroup";
-import FormLabel from "@material-ui/core/FormLabel";
-import TextField from "@material-ui/core/TextField";
+
+import TeamFields from "./TeamFields";
 
 const NewMatchForm = () => {
   const [team1Players, setTeam1Players] = useState([]);
-  const [team1Score, setTeam1Score] = useState(0);
   const [team2Players, setTeam2Players] = useState([]);
+
+  const [team1Score, setTeam1Score] = useState(0);
   const [team2Score, setTeam2Score] = useState(0);
 
   const [players, setPlayers] = useState([]);
@@ -32,19 +29,16 @@ const NewMatchForm = () => {
     fetchPlayers();
   }, []);
 
-  const handleUpdateScore = (value, updateFunc) => {
-    if (value < 0) value = 0;
-    updateFunc(value);
-  };
+  const handlePlayerUpdate = (teamArray, setTeamArray) => {
+    return e => {
+      const userId = parseInt(e.target.value);
 
-  const handlePlayerUpdate = (e, currentPlayers, updateFunc) => {
-    const userId = parseInt(e.target.value);
-
-    if (e.target.checked) {
-      updateFunc([userId, ...currentPlayers]);
-    } else {
-      updateFunc(currentPlayers.filter(p => !userId));
-    }
+      if (e.target.checked) {
+        setTeamArray([userId, ...teamArray]);
+      } else {
+        setTeamArray(teamArray.filter(p => p !== userId));
+      }
+    };
   };
 
   const handleSubmit = async () => {
@@ -71,63 +65,22 @@ const NewMatchForm = () => {
 
   return (
     <form action="post">
-      <TextField
-        type="number"
-        label="Mål lag 1"
-        variant="outlined"
-        value={team1Score}
-        onChange={e => handleUpdateScore(e.target.value, setTeam1Score)}
-        required
+      <TeamFields
+        teamNum={1}
+        teamScore={team1Score}
+        setTeamScore={setTeam1Score}
+        players={players}
+        opponents={team2Players}
+        handlePlayerUpdate={handlePlayerUpdate(team1Players, setTeam1Players)}
       />
-      <div />
-      <FormControl>
-        <FormLabel>Lag 1</FormLabel>
-        <FormGroup>
-          {players
-            .filter(p => !team2Players.includes(p.id))
-            .map(p => (
-              <FormControlLabel
-                key={p.id}
-                control={<Checkbox value={p.id} />}
-                label={p.username}
-                onChange={e =>
-                  handlePlayerUpdate(e, team1Players, setTeam1Players)
-                }
-              />
-            ))}
-        </FormGroup>
-      </FormControl>
-      <div />
-
-      <TextField
-        type="number"
-        min="1"
-        max="2"
-        step="1"
-        variant="outlined"
-        label="Mål lag 2"
-        value={team2Score}
-        onChange={e => handleUpdateScore(e.target.value, setTeam2Score)}
-        required
+      <TeamFields
+        teamNum={2}
+        teamScore={team2Score}
+        setTeamScore={setTeam2Score}
+        players={players}
+        opponents={team1Players}
+        handlePlayerUpdate={handlePlayerUpdate(team2Players, setTeam2Players)}
       />
-      <div />
-      <FormControl>
-        <FormLabel>Lag 2</FormLabel>
-        <FormGroup>
-          {players
-            .filter(p => !team1Players.includes(p.id))
-            .map(p => (
-              <FormControlLabel
-                key={p.id}
-                control={<Checkbox value={p.id} />}
-                label={p.username}
-                onChange={e =>
-                  handlePlayerUpdate(e, team2Players, setTeam2Players)
-                }
-              />
-            ))}
-        </FormGroup>
-      </FormControl>
       <div />
       <Button variant="contained" color="primary" onClick={handleSubmit}>
         Spara
---
+++ client/src/components/TeamFields.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControl from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormLabel from "@material-ui/core/FormLabel";
+import TextField from "@material-ui/core/TextField";
+
+const TeamFields = ({
+  teamNum,
+  teamScore,
+  setTeamScore,
+  players,
+  opponents,
+  handlePlayerUpdate,
+}) => {
+  const handleUpdateScore = value => {
+    let val = parseInt(value) || "";
+    if (val < 0) val = 0;
+    setTeamScore(val);
+  };
+
+  return (
+    <div>
+      <TextField
+        type="number"
+        label={`Mål lag ${teamNum}`}
+        variant="outlined"
+        value={teamScore}
+        onChange={e => handleUpdateScore(e.target.value)}
+        required
+      />
+      <FormControl>
+        <FormLabel>{`Lag ${teamNum}`}</FormLabel>
+        <FormGroup>
+          {players.map(p => (
+            <FormControlLabel
+              key={p.id}
+              control={<Checkbox value={p.id} />}
+              label={p.username}
+              onChange={e => handlePlayerUpdate(e, teamNum - 1)}
+              disabled={opponents.includes(p.id)}
+            />
+          ))}
+        </FormGroup>
+      </FormControl>
+    </div>
+  );
+};
+
+export default TeamFields;
