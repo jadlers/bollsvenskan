a07908e4f2c3dbac3e22ca8dfa15101b7c908117
Jacob Adlers <jacob.adlers@gmail.com>
2020-04-21 22:49:50 +0200

    Add loading spinner while fetching matches

--- src/components/LeagueOverview.js
+++ src/components/LeagueOverview.js
@@ -3,18 +3,36 @@ import React from "react";
 import ScoreBoard from "./ScoreBoard";
 import MatchesList from "./MatchesList";
 
+import Fade from "@material-ui/core/Fade";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
 function LeagueOverview(props) {
   const matches = props.matches;
 
-  return (
-    <div>
+  const loading = matches.length === 0;
+
+  return loading ? (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        marginTop: "2em",
+      }}
+    >
+      <Fade in={loading} timeout={{ appear: 800 }} unmountOnExit>
+        <CircularProgress />
+      </Fade>
+    </div>
+  ) : (
+    <>
       <ScoreBoard matches={matches} style={{ marginBottom: "2em" }} />
       <MatchesList
         matches={matches.sort((a, b) =>
           a.dotaMatchId <= b.dotaMatchId ? 1 : -1
         )}
       />
-    </div>
+    </>
   );
 }
 
