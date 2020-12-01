97a980631be9e8c423ad4bd561a5236d132ebf99
Jacob Adlers <jacob.adlers@gmail.com>
2020-12-01 02:34:08 +0100

    Add form for creating new praising phrases

--- src/pages/DotaFirstBloodPhrases.jsx
+++ src/pages/DotaFirstBloodPhrases.jsx
@@ -3,12 +3,19 @@ import { FirstBloodPhraseForm } from "../components/FirstBloodPhraseForm";
 
 function DotaFirstBloodPhrases() {
   return (
-    <div className="flex flex-col bg-white rounded shadow-2xl">
-      <p className="text-xl text-gray-800 text-center font-medium my-4">
-        Lägg till en ny retfull mening till de som dör först.
-      </p>
-      <FirstBloodPhraseForm phraseType="mock" />
-      {/* <FirstBloodPhraseForm phraseType="praise" /> */}
+    <div className="flex flex-col space-y-12">
+      <div className="bg-gray-50 rounded shadow-xl">
+        <p className="text-xl text-gray-800 text-center font-medium my-4">
+          Lägg till en ny retfull mening till de som dör först.
+        </p>
+        <FirstBloodPhraseForm phraseType="mock" />
+      </div>
+      <div className="bg-gray-50 rounded shadow-xl">
+        <p className="text-xl text-gray-800 text-center font-medium my-4">
+          Lägg till en ny mening till de som får det första blodet att spillas.
+        </p>
+        <FirstBloodPhraseForm phraseType="praise" />
+      </div>
     </div>
   );
 }
