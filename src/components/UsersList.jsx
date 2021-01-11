489a5535ae95f5fdf063dc82cf0b638bfad3d3ea
Jacob Adlers <jacob.adlers@gmail.com>
2021-01-11 23:41:00 +0100

    Make users list a tags

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,18 +1,19 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 function UsersList({ users, selectUser }) {
   users.sort((a, b) => a.id > b.id);
   return (
     <div>
       <p>List of users:</p>
-      <ul>
+      <ul className="space-y-1">
         {users.map((user) => (
-          <li
-            key={user.id}
-            className="px-2 py-1 rounded uppercase cursor-pointer hover:bg-nord-2 font-bold text-nord-8"
-            onClick={() => selectUser(user.id)}
-          >
-            {user.username}
+          <li key={user.id}>
+            <a
+              className="px-2 py-1 rounded uppercase cursor-pointer hover:bg-nord-2 font-bold text-nord-8"
+              onClick={() => selectUser(user.id)}
+            >
+              {user.username}
+            </a>
           </li>
         ))}
       </ul>
