b68c6f12668f17c0789fe005902d83a472689f24
Jacob Adlers <jacob.adlers@gmail.com>
2021-02-09 15:03:17 +0100

    Remove rounded corners

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
 export default function Card({ width, children }) {
-  return (
-    <div className={`bg-nord-1 rounded p-2 ${width || ""}`}>{children}</div>
-  );
+  return <div className={`bg-nord-1 p-2 ${width || ""}`}>{children}</div>;
 }
