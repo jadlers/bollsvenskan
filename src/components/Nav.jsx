58ba636e0ef21f1bdc32bb0009f536444a6df174
Jacob Adlers <jacob.adlers@gmail.com>
2024-10-15 23:24:14 +0200

    Remove link to sign up
    Handled with a Discord bot instead now

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,38 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { navigate } from "@reach/router";
-import { Button, ExternalLinkButton } from "./Buttons";
+import { Button } from "./Buttons";
 
 import ThemeToggler from "./ThemeToggler";
 
 function Nav() {
-  const baseUrl = process.env.REACT_APP_API_URL;
-  const [pollUrl, setPollUrl] = useState(
-    // Default to document with all links
-    "http://nextcloud.jacobadlers.com/index.php/s/nntLtmeAFytc3SW"
-  );
-
-  useEffect(() => {
-    const fetchSignupLinks = async () => {
-      try {
-        const res = await fetch(`${baseUrl}/dota/signup`);
-        if (!res.ok) {
-          return;
-        }
-
-        const body = await res.json();
-        if (body.currentPollUrl !== "") {
-          setPollUrl(body.currentPollUrl);
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    if (process.env.NODE_ENV !== "development") {
-      fetchSignupLinks();
-    }
-  }, [baseUrl]);
-
   const seasons = [1, 2, 3, 4, 5, 6, 7, 8];
 
   return (
@@ -74,13 +46,6 @@ function Nav() {
           >
             Roasts
           </Button>
-          <ExternalLinkButton
-            variant="primary"
-            hoverBg="theme-background-1"
-            href={pollUrl}
-          >
-            Anmälan
-          </ExternalLinkButton>
         </div>
       </div>
     </nav>
